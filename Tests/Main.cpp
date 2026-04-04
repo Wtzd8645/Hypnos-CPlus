9a92e63c96c5f18f7db159d2500ae9176bdfff1b
Neo <wtzd8645@gmail.com>
2026-04-05 05:06:26 +0800

    chore: Introduce CTest

--- Tests/Main.cpp
+++ Tests/Main.cpp
@@ -1,14 +1,26 @@
 #include "Network/NetworkTest.hpp"
+#include <cstdlib>
 #include <iostream>
 
 using namespace Blanketmen;
 
-int main()
+static int ReadInput(int argc, char* argv[])
+{
+    if (argc > 1)
+    {
+        return std::atoi(argv[1]);
+    }
+
+    int input = 0;
+    std::cin >> input;
+    return input;
+}
+
+int main(int argc, char* argv[])
 {
     Logging::Info("1: Network");
 
-    int input;
-    std::cin >> input;
+    int input = ReadInput(argc, argv);
     switch (input)
     {
         case 1:
